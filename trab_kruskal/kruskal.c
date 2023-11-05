fa98b9954c9aa24d0ffb211dd66181be0df50089
Nícolas <nicolasdamotaarruda@hotmail.com>
2023-11-04 23:08:35 -0300

    Consertando a lógica do rank e adaptando o código para ficar no formato desejado no trabalho (lendo entradas do usuário até o FEOF)

--- trab_kruskal/kruskal.c
+++ trab_kruskal/kruskal.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Utilizando conjuntos disjuntos para resolver este problema sobre grafos*/
+/*Utilizando conjuntos disjuntos para resolver este problema sobre conectividade em grafos*/
 typedef struct _Conjunto{
     struct _Conjunto* pai;    /*Ponteiro para o próximo elemento do conjunto*/
-    int relacoes;             /*Número de relações que este elemento possui*/
+    int rank;                 /*Funcionará como a profundidade do conjunto (apenas o rank do representante será usado nas operações)*/
 } Conjunto;
 
 /*Função para inicializar um conjunto*/
 Conjunto* Init() {
     Conjunto* ptr = malloc(sizeof(Conjunto));
     ptr->pai = ptr;
-    ptr->relacoes = 0;
+    ptr->rank = 0;
     return ptr;
 }
 
@@ -29,13 +29,13 @@ void Union(Conjunto* a, Conjunto* b){
     Conjunto* RepA = Find(a);
     Conjunto* RepB = Find(b);
 
-    if(RepA->relacoes > RepB->relacoes) {
+    if(RepA->rank > RepB->rank) 
         RepB->pai = RepA;
-        RepA->relacoes++;
-    }
     else{
+        if (RepA->rank == RepB->rank)
+            RepB->rank++;    /*Só incremento o rank caso os ranks dos representantes forem iguais*/
+
         RepA->pai = RepB;
-        RepB->relacoes++;
     }
 }
 
@@ -52,22 +52,20 @@ int main() {
 
     scanf("%d", &numVertices);
     conjunto = (Conjunto **) malloc(sizeof(Conjunto *) * numVertices);
+
+    /*Criando um conjunto disjunto para cada vértice*/
     for (i = 0; i < numVertices; i++){
         conjunto[i] = Init();
     }
 
-    /*while (scanf("%d", &v1) != EOF) {*/      /*Loop para os inputs do usuário*/
-    while (1) {
-        scanf("%d %d %d", &v1, &v2, &pesoAresta);
-        if(v1 == 99)
-            break;
-        if(!isRelacionado(conjunto[v1-1], conjunto[v2-1])) {   /*Se os elementos já estiverem no mesmo conjunto, não vou uni-los (eu só preciso desta verificação. Como os pesos das arestas são fornecidos de forma crescente, eu tenho certeza de que, se já existe relação entre dois elementos, esta relação já tem caminho mínimo)*/
+    while (scanf("%d", &v1) != EOF) {      /*Loop para os inputs do usuário (encerra com EOF)*/
+        scanf("%d %d", &v2, &pesoAresta);
+        if(!isRelacionado(conjunto[v1-1], conjunto[v2-1])) {   /*Se os elementos já estiverem no mesmo conjunto, não vou uni-los (Eu preciso apenas desta verificação. Como os pesos das arestas são fornecidos de forma crescente, eu tenho certeza de que, se já existe relação entre dois elementos, esta relação já tem caminho mínimo)*/
             Union(conjunto[v1-1], conjunto[v2-1]);
             somaPesos += pesoAresta;
         }
     }
 
     printf("%d\n", somaPesos);
-
     return 0;
 }
